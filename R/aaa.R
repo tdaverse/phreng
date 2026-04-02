cc9dc0838f27c2b35249011bf130429010ae959e
ethanborick <eborick@ufl.edu>
2026-04-02 11:51:37 -0400

    added source code

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,9 @@
 #' importFrom S7 new_property
 new_property <- S7::new_property
 class_character <- S7::class_character
+class_double <- S7::class_double
+new_class <- S7::new_class
+class_logical <- S7::class_logical
+new_S3_class <- S7::new_S3_class
+new_generic <- S7::new_generic
+`method<-` <- S7::`method<-`
--- R/current_double_dispatch_solution.R
+++ R/current_double_dispatch_solution.R
@@ -1,8 +1,5 @@
 #' include aaa.r
 
-# allow different spacing, capitalization, etc
-# e.g. allow `<S7_obj>(filtration = "Alpha shape")`
-# maybe use `tolower()`, `snakecase::to_snake_case()`, etc.
 filtration_type <- new_property(
   class = class_character,
   validator = function(value) {
@@ -13,3 +10,199 @@ filtration_type <- new_property(
   },
   default = "vietoris_rips"
 )
+
+max_dimension_type <- new_property(
+  class = class_double,
+  validator = function(value) {
+    if (!is.na(value) & value < 0)
+      "must be a non-negative integer"
+    if (value %% 1 != 0)
+      "must be a non-negative integer"
+  },
+  default = 1
+)
+
+engine_type <- new_property(
+  class = class_character,
+  validator = function(value) {
+    if (  !(value %in% c("TDA","ripserr")) )
+      "must be TDA or ripserr"
+  },
+  default ="TDA"
+)
+
+library_type <- new_property(
+  class = class_character,
+  validator = function(value) {
+    if (  !(value %in% c("GUDHI", "PHAT", "Dionysus", NA_character_)) )
+      "must be GUDHI, PHAT, or Dionysus or NA_character_"
+  },
+  default = NA_character_
+)
+
+max_diameter_type <- new_property(
+  class = class_double,
+  default = NA_real_)
+
+
+max_radius_type <- new_property(
+  class = class_double,
+  getter = function(self) {
+    self@max_diameter/2
+  }
+)
+
+sublevel_type <- new_property(
+  class = class_logical,
+  validator = function(value) {
+    if (length(value) != 1 || is.na(value))
+      "sublevel must be either TRUE or FALSE."
+  },
+  default = TRUE
+)
+
+max_scale_type = new_property(
+  class = class_double,
+  default = NA_real_
+)
+
+PH <- new_class("PH",
+                properties = list(
+                  filtration = filtration_type,
+                  engine = engine_type,
+                  library = library_type,
+                  max_dimension = max_dimension_type),
+                validator = function(self) {
+                  if (self@engine == "ripserr" & !is.na(self@library) ){
+                    sprintf("Library is only defined when engine is TDA. Please leave library blank or NA_character_ when using the ripserr engine.")
+                  }
+                }
+)
+
+PH_pointcloud <-  new_class("PH_pointcloud", parent = PH,
+                            properties = list(
+                              max_radius = max_radius_type,
+                              max_diameter= max_diameter_type),
+                            validator = function(self) {
+                              if (self@engine == "ripserr" & (self@filtration == "alpha_complex" || self@filtration=="alpha_shape")  ){
+                                sprintf("Alpha complexes are only defined for the engine TDA using point clouds. Please use library TDA for any alpha filtration")
+                              }
+                            }
+)
+
+PH_raster <- new_class("PH_raster", parent = PH,
+                       properties = list(
+                         max_scale = max_scale_type,
+                         sublevel = sublevel_type),
+                       validator = function(self) {
+                         if (self@filtration != "cubical") {
+                           sprintf("Only cubical filtrations are allowed for raster objects")
+                         }
+                       }
+)
+
+dist_class <- new_S3_class("dist")
+
+compute_persistence <- new_generic("compute_persistence", c("object","data"))
+
+method(compute_persistence, list(PH_pointcloud, dist_class)) <- function(object, data) {
+  res <- NULL
+  if (object@engine == "ripserr") {
+    res <-  vietoris_rips(
+      data,
+      max_dim = object@max_dimension,
+      threshold = ifelse(is.na(object@max_diameter), 0, object@max_diameter)
+    ) |> as_persistence()
+  }
+  else if (object@engine == "TDA") {
+    if (object@filtration == "vietoris_rips") {
+      res<- ripsDiag(
+        data,
+        library = ifelse(is.na(object@library), "GUDHI", object@library),
+        maxdimension = object@max_dimension,
+        dist = "arbitrary",
+        maxscale = ifelse(is.na(object@max_diameter), 0, object@max_diameter)
+      ) |> as_persistence()
+    }
+    if (object@filtration == "alpha_complex") {
+      res<- alphaComplexDiag(
+        data,
+        library = ifelse(is.na(object@library), c("GUDHI", "Dionysus"), object@library),
+        maxdimension = object@max_dimension
+      ) |> as_persistence()
+    }
+    if (object@filtration == "alpha_shape") {
+      res<-alphaShapeDiag(
+        data,
+        library = ifelse(is.na(object@library), c("GUDHI", "Dionysus"), object@library),
+        maxdimension = object@max_dimension
+      ) |> as_persistence()
+    }
+  }
+  res
+}
+
+method(compute_persistence, list(PH_pointcloud, class_double)) <- function(object, data) {
+  res <- NULL
+  if (is.matrix(data) | is.array(data)) {
+    if (object@engine == "ripserr") {
+      res <- vietoris_rips(
+        data,
+        max_dim = object@max_dimension,
+        threshold = ifelse(is.na(object@max_diameter), 0, object@max_diameter)
+      ) |> as_persistence()
+    }
+    else if (object@engine == "TDA") {
+      if (object@filtration == "vietoris_rips") {
+        res <- ripsDiag(
+          data,
+          library = ifelse(is.na(object@library), "GUDHI", object@library),
+          maxdimension = object@max_dimension,
+          maxscale = ifelse(is.na(object@max_diameter), 0, object@max_diameter)
+        ) |> as_persistence()
+      }
+      if (object@filtration == "alpha_complex") {
+        res<- alphaComplexDiag(
+          data,
+          library = ifelse(is.na(object@library), c("GUDHI", "Dionysus"), object@library),
+          maxdimension = object@max_dimension
+        ) |> as_persistence()
+      }
+      if (object@filtration == "alpha_shape") {
+        res <- alphaShapeDiag(
+          data,
+          library = ifelse(is.na(object@library), c("GUDHI", "Dionysus"), object@library),
+          maxdimension = object@max_dimension
+        ) |> as_persistence()
+      }
+    }
+    res
+  }
+  else {
+    return("Data must be a matrix or an array for PH_pointcloud")
+  }
+}
+
+method(compute_persistence, list(PH_raster, class_double)) <- function(object, data) {
+  res <- NULL
+  if (is.matrix(data) | is.array(data)) {
+    if (object@engine == "ripserr") {
+      res <- cubical(
+        data,
+        threshold = ifelse(is.na(object@max_scale), 0, object@max_scale)
+      ) |> as_persistence()
+    }
+    else if (object@engine == "TDA") {
+      res <- gridDiag(
+        FUNvalues = data,
+        library = ifelse(is.na(object@library), "GUDHI", object@library),
+        sublevel = object@sublevel,
+        maxdimension = object@max_dimension
+      ) |> as_persistence()
+    }
+    res
+  }
+  else {
+    return("Data must be a matrix or an array for PH_pointcloud")
+  }
+}
