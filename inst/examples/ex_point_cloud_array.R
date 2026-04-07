fcdfdbe8a984975ad2c9d3faef795719f174ee92
megan-sin <megansin@ufl.edu>
2026-04-07 11:28:25 -0400

    add point cloud array object examples

---
+++ inst/examples/ex_point_cloud_array.R
@@ -0,0 +1,26 @@
+# generating point cloud data
+data <- matrix(rnorm(100), ncol = 4)
+
+# with TDA
+## vietoris_rips filtration
+x <- PH_pointcloud(
+  filtration = "vietoris_rips", 
+  engine = "TDA", 
+  library = "GUDHI",
+  max_dimension = 1,
+  max_diameter = 1000
+  )
+result <- compute_persistence(x, data)
+as.data.frame(result)
+
+# with ripserr
+## vietoris_rips filtration  
+x <- PH_pointcloud(
+  filtration = "vietoris_rips",
+  engine = "ripserr",
+  library = ,
+  max_dimension = 1,
+  max_diameter = 1000
+  )
+result <- compute_persistence(x, data)
+as.data.frame(result)
